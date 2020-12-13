55f8668c66f6c3e102466271a6e78f4085cf4637
Nurettin <abacinurettin@gmail.com>
2020-12-14 01:56:52 +0300

    Multiplayer connection is integrated successfully.

--- app/chat/consumers.py
+++ app/chat/consumers.py
@@ -26,16 +26,22 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         message_type = text_data_json['type']
 
-        if message_type == "coordinate":
+        if message_type == "stroke_coordinate":
             coordinate = text_data_json['coordinate']
             print(coordinate)
+
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'send_coordinate',
+                    'coordinate': coordinate
+                }
+            )
 
 
         elif message_type == 'message':
             message = text_data_json['text']
-            print('mesaj alindi hocam', message)
 
-            # Send message to room group
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
@@ -44,13 +50,88 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 }
             )
 
+        elif message_type == 'prevCoordinate':
+            coordinate = text_data_json['coordinate']
+            print(coordinate)
 
-    # Receive message from room group
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'prev_coordinate',
+                    'coordinate': coordinate
+                }
+            )
+
+        elif message_type == 'brushColor':
+            color = text_data_json['color']
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'change_color',
+                    'color': color
+                }
+            )
+
+        elif message_type == 'strokeSize':
+            size = text_data_json['size']
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    "type": 'change_size',
+                    'size': size
+                }
+            )
+
+        elif message_type == 'clearCanvas':
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    "type": 'clear_canvas'
+                }
+            )
+
+    # Send messages
     async def chat_message(self, event):
         message = event['message']
 
-        # Send message to WebSocket
         await self.send(text_data=json.dumps({
+            'type': 'chat_message',
             'message': message
         }))
 
+    async def send_coordinate(self, event):
+        coordinate = event['coordinate']
+
+        await self.send(text_data=json.dumps({
+            'type': 'coordinate',
+            'coordinate': coordinate
+        }))
+
+    async def prev_coordinate(self, event):
+        coordinate = event['coordinate']
+
+        await self.send(text_data=json.dumps({
+            'type': 'prevCoordinate',
+            'coordinate': coordinate
+        }))
+
+    async def change_color(self, event):
+        color = event['color']
+
+        await self.send(text_data=json.dumps({
+            'type': 'brushColor',
+            'color': color
+        }))
+
+    async def change_size(self, event):
+        size = event['size']
+
+        await self.send(text_data=json.dumps({
+            'type': 'change_size',
+            'size': size
+        }))
+
+    async def clear_canvas(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'clear_canvas'
+        }))
